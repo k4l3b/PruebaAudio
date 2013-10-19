539961af65bf386c8021bdffb8674cd46e10c9ad
k4l3b <carlosgabrielvergara@gmail.com>
2013-10-19 18:08:26 -0300

    Commit inicial.

---
+++ gen/com/boondocksaints/test/pruebaaudio/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.boondocksaints.test.pruebaaudio;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
